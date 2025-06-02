9072bc66e1d7035c4921becb86520856343b593e
Fidel-Blop <fidelllgarcia16@gmail.com>
2025-06-02 15:20:21 -0300

    Update fun-memes.js

--- plugins/fun-memes.js
+++ plugins/fun-memes.js
@@ -1,14 +1,21 @@
 import hispamemes from 'hispamemes'
+
 let handler = async (m, { conn, usedPrefix, command }) => {
-const meme = hispamemes.meme()
-conn.sendFile(m.chat, meme, '', '', fkontak)
-m.react(emoji2)
+  try {
+    const meme = await hispamemes.meme()
+    const caption = `*🎭 MEME ENTREGADO POR FREDDY FAZBEAR™️*\n\nPrepárate para reír... antes de que apaguen las luces.`
+    await conn.sendFile(m.chat, meme, 'meme.jpg', caption, m)
+    await m.react('👻') // Reacción estilo FNaF
+  } catch (e) {
+    await m.reply('💀 Error... parece que Foxy desconectó el servidor de memes.')
+  }
 }
-handler.help = ['meme']
+
+handler.help = ['meme', 'memes']
 handler.tags = ['fun']
 handler.command = ['meme', 'memes']
 handler.coin = 1
-handler.group = true;
+handler.group = true
 handler.register = true
 
 export default handler
