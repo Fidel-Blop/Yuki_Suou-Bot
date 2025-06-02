b99924fe8219d9c9c97ec74c240d1858de666fa5
Fidel-Blop <fidelllgarcia16@gmail.com>
2025-06-01 23:24:53 -0300

    Update grupo-setwelcome.js

--- plugins/grupo-setwelcome.js
+++ plugins/grupo-setwelcome.js
@@ -1,15 +1,21 @@
-let handler = async (m, { conn, text, isRowner }) => {
-  if (!text) return m.reply(`${emoji} Por favor, proporciona una bienvenida para el bot.\n> Ejemplo: #setwelcome Hola user`);
+let handler = async (m, { conn, text }) => {
+  if (!text) {
+    return m.reply(
+      '🛑 *Sistema de Gestión - FNaF LATAM*\n\n⚠️ No se detectó ninguna frase.\n\n📌 Usa:\n#setwelcome Bienvenido, user, a Freddy Fazbear\'s Pizzería.\n\n🔒 Solo administradores autorizados pueden cambiar el mensaje de bienvenida.'
+    );
+  }
 
   global.welcom1 = text.trim();
-  
-  m.reply(`${emoji} La bienvenida del bot ha sido cambiado a: ${global.welcom1}`);
+
+  m.reply(
+    `✅ *Actualización completada*\n\n📂 Mensaje de bienvenida establecido como:\n\n「 ${global.welcom1} 」\n\n🎮 *Sistema de Entrada Automatizada – FNaF LATAM*`
+  );
 };
 
 handler.help = ['setwelcome'];
-handler.tags = ['tools'];
+handler.tags = ['grupo'];
 handler.command = ['setwelcome'];
-handler.owner = false;
 handler.admin = true;
+handler.botAdmin = true;
 
 export default handler;
