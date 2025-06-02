8ce63f8c105f171934a68258b158ba89069fa861
Fidel-Blop <fidelllgarcia16@gmail.com>
2025-06-02 16:00:50 -0300

    Update grupo-setbye.js

--- plugins/grupo-setbye.js
+++ plugins/grupo-setbye.js
@@ -1,15 +1,25 @@
 let handler = async (m, { conn, text, isRowner }) => {
-  if (!text) return m.reply(`${emoji} Por favor, proporciona un mensaje de despedida para el bot.\n> Ejemplo: #setbye adios user`);
+  const consola = '📡'
+  const emoji = '👋'
+  const denegado = '❌'
 
-  global.welcom2 = text.trim();
-  
-  m.reply(`${emoji} La despedida del bot ha sido cambiado a: ${global.welcom2}`);
-};
+  if (!text) {
+    return m.reply(
+      `${denegado} *SISTEMA FNaF LATAM - CONFIGURACIÓN DE DESPEDIDA*\n\n🛑 Debes proporcionar un mensaje de despedida.\n\n📌 *Ejemplo:*\n#setbye Adiós @user, te estaremos vigilando desde las cámaras... 👁️`
+    )
+  }
 
-handler.help = ['setdespedida'];
-handler.tags = ['tools'];
-handler.command = ['setbye'];
-handler.owner = false;
-handler.admin = true;
+  global.welcom2 = text.trim()
 
-export default handler;
+  m.reply(
+    `${consola} *PROTOCOLO DE SALIDA CONFIGURADO*\n\n${emoji} Nuevo mensaje de despedida cargado exitosamente:\n\n📝 _${global.welcom2}_\n\n📼 La cinta ha sido almacenada en el sistema central.`
+  )
+}
+
+handler.help = ['setbye']
+handler.tags = ['tools']
+handler.command = ['setbye']
+handler.owner = false
+handler.admin = true
+
+export default handler
