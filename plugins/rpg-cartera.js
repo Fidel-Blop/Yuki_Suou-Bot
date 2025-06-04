bd81ad97504ad26b1524c4e97975a72764b32348
Fidel-Blop <fidelllgarcia16@gmail.com>
2025-06-04 16:13:52 -0300

    Update rpg-cartera.js

--- plugins/rpg-cartera.js
+++ plugins/rpg-cartera.js
@@ -1,14 +1,25 @@
-let handler = async (m, {conn, usedPrefix}) => {
-let who = m.mentionedJid[0] ? m.mentionedJid[0] : m.quoted ? m.quoted.sender : m.sender
-if (who == conn.user.jid) return error 
-if (!(who in global.db.data.users)) return conn.reply(m.chat, `${emoji4} El usuario no se encuentra en mi base de Datos.`, m)
-let user = global.db.data.users[who]
-await m.reply(`${who == m.sender ? `Tienes *${user.coin} ${moneda} 💸* en tu Cartera` : `El usuario @${who.split('@')[0]} tiene *${user.coin} ${moneda} 💸* en su Cartera`}. `, null, { mentions: [who] })}
+let handler = async (m, { conn, usedPrefix }) => {
+    const emoji4 = '📵';
+    const moneda = '🪙';
 
-handler.help = ['wallet']
-handler.tags = ['economy']
-handler.command = ['wallet', 'cartera']
-handler.group = true
-handler.register = true
+    let who = m.mentionedJid[0] ? m.mentionedJid[0] : m.quoted ? m.quoted.sender : m.sender;
 
-export default handler
+    if (who == conn.user.jid) return m.reply('❌ *¡No puedes espiar mi cartera, guardia nocturno!*');
+    if (!(who in global.db.data.users)) 
+        return conn.reply(m.chat, `${emoji4} *Usuario no registrado en los archivos del sistema...*\n_Tal vez desapareció como los demás..._`, m);
+
+    let user = global.db.data.users[who];
+    let texto = who == m.sender
+        ? `🎭 *Cartera Digital - FNaF LATAM*\n\n🔦 Guardián: *${conn.getName(who)}*\n💸 Dinero en mano: *${user.coin} ${moneda}*\n\n_Si el sistema falla... esta es tu única esperanza._`
+        : `🎭 *Cartera Detectada*\n\n👤 Usuario: *@${who.split('@')[0]}*\n💰 Fondos visibles: *${user.coin} ${moneda}*\n\n_Todo queda registrado... incluso esto._`;
+
+    await conn.reply(m.chat, texto, null, { mentions: [who] });
+};
+
+handler.help = ['wallet'];
+handler.tags = ['economy'];
+handler.command = ['wallet', 'cartera'];
+handler.group = true;
+handler.register = true;
+
+export default handler;
