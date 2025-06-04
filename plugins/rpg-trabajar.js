bfa1a7fe1a7e08c46aaf88b09b7f5618bffeb049
Fidel-Blop <fidelllgarcia16@gmail.com>
2025-06-04 16:52:39 -0300

    Update rpg-trabajar.js

--- plugins/rpg-trabajar.js
+++ plugins/rpg-trabajar.js
@@ -1,82 +1,75 @@
 let cooldowns = {}
 
 let handler = async (m, { conn, isPrems }) => {
-let user = global.db.data.users[m.sender]
-let tiempo = 5 * 60
-if (cooldowns[m.sender] && Date.now() - cooldowns[m.sender] < tiempo * 1000) {
-const tiempo2 = segundosAHMS(Math.ceil((cooldowns[m.sender] + tiempo * 1000 - Date.now()) / 1000))
-conn.reply(m.chat, `${emoji3} Debes esperar *${tiempo2}* para usar *#w* de nuevo.`, m)
-return
-}
-let rsl = Math.floor(Math.random() * 500)
-cooldowns[m.sender] = Date.now()
-await conn.reply(m.chat, `${emoji} ${pickRandom(trabajo)} *${toNum(rsl)}* ( *${rsl}* ) ${moneda} 💸.`, m)
-user.coin += rsl
+  let emoji = '💼'
+  let emoji2 = '💸'
+  let emoji3 = '⏳'
+  let moneda = 'monedas'
+
+  let user = global.db.data.users[m.sender]
+  let senderName = await conn.getName(m.sender)
+  let tiempo = 5 * 60 // 5 minutos
+
+  if (cooldowns[m.sender] && Date.now() - cooldowns[m.sender] < tiempo * 1000) {
+    const tiempoRestante = segundosAHMS(Math.ceil((cooldowns[m.sender] + tiempo * 1000 - Date.now()) / 1000))
+    return conn.reply(m.chat, `❌ ${emoji3} *${senderName}*, ya chambeaste hace poco.\n⏱️ Vuelve en *${tiempoRestante}* para seguirle metiendo duro.`, m)
+  }
+
+  let ganancias = Math.floor(Math.random() * 500)
+  cooldowns[m.sender] = Date.now()
+  user.coin += ganancias
+
+  let trabajoElegido = pickRandom([
+    '🧹 Barriste los pasillos embrujados de Freddy Fazbear’s 🍕',
+    '🧑‍🍳 Cocinaste pizzas para los animatrónicos con hambre 😋',
+    '👷 Arreglaste los cables de seguridad con miedo al susto 👻',
+    '🧑‍🎨 Pintaste los posters vintage del show 🎨',
+    '🔧 Reparaste a Foxy antes de que se enoje 😡',
+    '🛠️ Le diste mantenimiento al generador (casi te electrocuta) ⚡',
+    '🎭 Actuaste en el show nocturno como animatrónico 🐻',
+    '🧼 Limpiando vómito de niño asustado... asquito 🤢',
+    '🕹️ Atendiste las maquinitas arcade como buen gamer 🎮'
+  ])
+
+  await conn.reply(m.chat, `
+╔═════🎪 𝙁𝙉𝘼𝙁 𝙇𝘼𝙏𝘼𝙈 𝙒𝙊𝙍𝙆 🎪═════╗
+┃ ${emoji} *Trabajo Realizado:*
+┃ ${trabajoElegido}
+┃ 
+┃ ${emoji2} *Ganancia:* +${toNum(ganancias)} ${moneda}
+╚════════════════════════╝`, m)
 }
 
 handler.help = ['trabajar']
 handler.tags = ['economy']
-handler.command = ['w','work','chambear','chamba', 'trabajar']
-handler.group = true;
-handler.register = true;
+handler.command = ['w','work','chambear','chamba','trabajar']
+handler.group = true
+handler.register = true
 
 export default handler
 
 function toNum(number) {
-if (number >= 1000 && number < 1000000) {
-return (number / 1000).toFixed(1) + 'k'
-} else if (number >= 1000000) {
-return (number / 1000000).toFixed(1) + 'M'
-} else if (number <= -1000 && number > -1000000) {
-return (number / 1000).toFixed(1) + 'k'
-} else if (number <= -1000000) {
-return (number / 1000000).toFixed(1) + 'M'
-} else {
-return number.toString()}}
-
-function segundosAHMS(segundos) {
-let minutos = Math.floor((segundos % 3600) / 60)
-let segundosRestantes = segundos % 60
-return `${minutos} minutos y ${segundosRestantes} segundos`
+  if (number >= 1000 && number < 1000000) {
+    return (number / 1000).toFixed(1) + 'k'
+  } else if (number >= 1000000) {
+    return (number / 1000000).toFixed(1) + 'M'
+  } else if (number <= -1000 && number > -1000000) {
+    return (number / 1000).toFixed(1) + 'k'
+  } else if (number <= -1000000) {
+    return (number / 1000000).toFixed(1) + 'M'
+  } else {
+    return number
+  }
 }
 
 function pickRandom(list) {
-return list[Math.floor(list.length * Math.random())];
+  return list[Math.floor(Math.random() * list.length)]
 }
 
-// Thanks to FG98
-const trabajo = [
-   "Trabajas como cortador de galletas y ganas",
-   "Trabaja para una empresa militar privada, ganando",
-   "Organiza un evento de cata de vinos y obtienes",
-   "Limpias la chimenea y encuentras",
-   "Desarrollas juegos para ganarte la vida y ganas",
-   "Trabajaste en la oficina horas extras por",
-   "Trabajas como secuestrador de novias y ganas",
-   "Alguien vino y representó una obra de teatro. Por mirar te dieron",
-   "Compraste y vendiste artículos y ganaste",
-   "Trabajas en el restaurante de la abuela como cocinera y ganas",
-   "Trabajas 10 minutos en un Pizza Hut local. Ganaste",
-   "Trabajas como escritor(a) de galletas de la fortuna y ganas",
-   "Revisas tu bolso y decides vender algunos artículos inútiles que no necesitas. Resulta que toda esa basura valía",
-   "Desarrollas juegos para ganarte la vida y ganas",
-   "Trabajas todo el día en la empresa por",
-   "Diseñaste un logo para una empresa por",
-   "¡Trabajó lo mejor que pudo en una imprenta que estaba contratando y ganó su bien merecido!",
-   "Trabajas como podador de arbustos y ganas",
-   "Trabajas como actor de voz para Bob Esponja y te las arreglaste para ganar",
-   "Estabas cultivando y Ganaste",
-   "Trabajas como constructor de castillos de arena y ganas",
-   "Trabajas como artista callejera y ganas",
-   "¡Hiciste trabajo social por una buena causa! por tu buena causa Recibiste",
-   "Reparaste un tanque T-60 averiado en Afganistán. La tripulación te pagó",
-   "Trabajas como ecologista de anguilas y ganas",
-   "Trabajas en Disneyland como un panda disfrazado y ganas",
-   "Reparas las máquinas recreativas y recibes",
-   "Hiciste algunos trabajos ocasionales en la ciudad y ganaste",
-   "Limpias un poco de moho tóxico de la ventilación y ganas",
-   "Resolviste el misterio del brote de cólera y el gobierno te recompensó con una suma de",
-   "Trabajas como zoólogo y ganas",
-   "Vendiste sándwiches de pescado y obtuviste",
-   "Reparas las máquinas recreativas y recibes",
-] 
+function segundosAHMS(segundos) {
+  segundos = Number(segundos)
+  const h = Math.floor(segundos / 3600)
+  const m = Math.floor((segundos % 3600) / 60)
+  const s = Math.floor((segundos % 3600) % 60)
+  return `${h > 0 ? h + 'h ' : ''}${m > 0 ? m + 'm ' : ''}${s}s`
+}
